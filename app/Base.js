143d4b1b9679c0a9692f97fb4483354e453da5d6
git-onepixel <846435237@qq.com>
2017-06-17 21:46:42 +0800

    实现数组去重

--- app/Base.js
+++ app/Base.js
@@ -1,15 +1,30 @@
-Wax.define([],function(){
-    
+Wax.define([], function() {
+
+    Array.prototype.unique = function(fn) {
+        var rst = [];
+        var tmp = {};
+        this.forEach(function(val) {
+            var key = 'uniq' + (typeof fn === 'function' ? fn.call(null, val) : val);
+            if (!tmp.hasOwnProperty(key)) {
+                rst.push(val);
+                tmp[key] = null;
+            }
+        }, this);
+
+        return rst;
+    }
+
+
     return {
         /**
          * APP版本号
          */
-        version:'1.0.0',
+        version: '1.0.0',
 
         /**
          * 系统平台
          */
-        platform:(function(){
+        platform: (function() {
             var u = navigator.userAgent;
             return {
                 iOS: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/),
@@ -22,25 +37,26 @@ Wax.define([],function(){
          * @param name
          * @returns {*}
          */
-        getUrlParam:function(name){
-            var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+        getUrlParam: function(name) {
+            var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
             var r = window.location.search.substr(1).match(reg);
-            if(r!=null)return  unescape(r[2]); return null;
+            if (r != null) return unescape(r[2]);
+            return null;
         },
 
         /**
          * 记录UBT
          */
-        sendUbt:function(){
+        sendUbt: function() {
             console.log('Base.sendUbt is invoked !')
         },
 
         /**
          * 记录日志
          */
-        sendLog:function(){
+        sendLog: function() {
             //app log
         }
-         
+
     }
 });
--- app/MainController.js
+++ app/MainController.js
@@ -1,22 +1,37 @@
-Wax.define([],function(){
+Wax.define([], function() {
     return {
-        onCreate:function(){
+        onCreate: function() {
             this.super.onCreate();
-            this.vm = this.createVue('#viewport',{
-                data:{
-                   welcome:'Hello,Vue!'
+            this.vm = this.createVue('#viewport', {
+                data: {
+                    welcome: 'Hello,Vue!'
                 }
             });
         },
 
-        onShow:function(){
+        onShow: function() {
             this.super.onShow();
-            console.log('App Info -> v'+ this.version,(this.platform.iOS?'iOS':'Android'));
+            console.log('App Info -> v' + this.version, (this.platform.iOS ? 'iOS' : 'Android'));
+
+            //测试数组的去重方法unique - 简单数据类型
+            var arr = [1, 2, 3, 2, 3, 5, 7];
+            console.log('简单数组去重 -> ', arr.unique())
+
+            //测试数组的去重方法unique - 对象类型
+            var arr2 = [
+                { id: 2 }, { id: 4 }, { id: 3 }, { id: 3 }, { id: 4 }, { id: 6 }
+            ]
+            var tmprst = arr2.unique(function(v) {
+                console.log(this) //使用了bind方法，JS引擎会忽略通过call或apply传递进来的this
+                return v.id
+            }.bind(this));
+
+            console.log('对象数组去重 -> ', tmprst)
         },
 
-        onHide:function(){
+        onHide: function() {
             this.super.onHide()
         }
     }
 
-},'Wax.app.Controller');
+}, 'Wax.app.Controller');
